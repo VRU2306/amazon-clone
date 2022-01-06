51b73c09416dca53ae68dbc249190c9991bc792e
VRUJESH H M <60779508+VRU2306@users.noreply.github.com>
2022-01-06 14:25:38 +0530

    small changes

--- src/Payment/axios.js
+++ src/Payment/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: 
-  // "https://us-central1-clone-7473c.cloudfunctions.net/api",
- 'http://localhost:5001/clonefeatures-46470/us-central1/api'
+  // "https://us-central1-clonefeatures-46470.cloudfunctions.net/api",
+ "https://amazone-cloneds.herokuapp.com/"
 });
 
 export default instance;
